460ff8b7720fbda6643c70bd9f36a0fcd8734613
Дмитрий <dmitrij@MacBook-Air-Dmitrij.local>
2026-01-20 22:54:59 +0300

    0.6.7

---
+++ build/linux/CMakeFiles/Blockchain_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Blockchain_autogen/mocs_compilation.cpp"
+  "Blockchain_autogen/timestamp"
+  "CMakeFiles/Blockchain_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Blockchain_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
